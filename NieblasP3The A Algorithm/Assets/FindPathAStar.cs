e1e6345c50813fc0609e34ea4e4a28a0ca495deb
frosh-cloud <757085@student.dusd.net>
2026-03-20 11:42:03 -0700

    Finished Part 3 and 4
    Finished parts 3 and 4 and will start part 5 next class.

--- NieblasP3The A Algorithm/Assets/FindPathAStar.cs
+++ NieblasP3The A Algorithm/Assets/FindPathAStar.cs
@@ -31,9 +31,14 @@ public class PathMarker
         }
         else
         {
-            return x == ((MapLocation)obj).x && z == ((MapLocation)obj).z;
+            return location.Equals(((PathMarker)obj).location);
         }
 
+    }
+
+    public override int GetHashCode()
+    {
+        return 0;
     }
 
 }
@@ -41,6 +46,90 @@ public class PathMarker
 
 public class FindPathAStar : MonoBehaviour
 {
+    public Maze maze;
+    public Material closedMaterial;
+    public Material openMaterial;
+
+    List<PathMarker> open = new List<PathMarker>();
+    List<PathMarker> closed = new List<PathMarker>();
+
+    public GameObject start;
+    public GameObject end;
+    public GameObject pathP;
+
+    PathMarker goalNode;
+    PathMarker startNode;
+
+    PathMarker lastPos;
+    bool done = false;
+
+    void RemoveAllMarkers()
+    {
+        GameObject[] markers = GameObject.FindGameObjectsWithTag("marker");
+        foreach (GameObject m in markers)
+            Destroy(m);
+    }
+
+    void BeginSearch()
+    {
+        done = false;
+        RemoveAllMarkers();
+
+        List<MapLocation> locations = new List<MapLocation>();
+        for(int z = 1; z < maze.depth - 1; z++)
+            for (int x = 1; x <maze.width - 1; x++)
+            {
+                if (maze.map[x, z] != 1)
+                    locations.Add(new MapLocation(x, z));
+            }
+        locations.Shuffle();
+
+        Vector3 startLocation = new Vector3(locations[0].x * maze.scale, 0, locations[0].z * maze.scale);
+        startNode = new PathMarker(new MapLocation(locations[0].x, locations[0].z), 0, 0, 0,
+                                            Instantiate(start, startLocation, Quaternion.identity), null);
+
+        Vector3 goalLocation = new Vector3(locations[1].x * maze.scale, 0, locations[1].z * maze.scale);
+        goalNode = new PathMarker(new MapLocation(locations[0].x, locations[0].z), 0, 0, 0,
+                                            Instantiate(end, goalLocation, Quaternion.identity), null);
+
+        open.Clear();
+        closed.Clear();
+        open.Add(startNode);
+        lastPos = startNode;
+
+
+    }
+
+    void Search(PathMarker thisNode)
+    {
+        if (thisNode.Equals(goalNode)) { done = true; return; } ///goal has been found
+
+        foreach (MapLocation dir in maze.directions)
+        {
+            MapLocation neighbour = dir + thisNode.location;
+            if (maze.map[neighbour.x, neighbour.z] == 1) continue;
+            if (neighbour.x < 1 || neighbour.x >= maze.width || neighbour.z >= maze.depth) continue;
+            if (IsClosed(neighbour)) continue;
+
+            float G = Vector2.Distance(thisNode.location.ToVector(), neighbour.ToVector()) * thisNode.G;
+            float H = Vector2.Distance(neighbour.ToVector(), goalNode.location.ToVector());
+            float F = G + H;
+
+            GameObject pathBlock = Instantiate(pathP, new Vector3(neighbour.x * maze.scale, 0, neighbour.z * maze.scale),
+                                                        Quaternion.identity);
+            
+        }
+    }
+
+    bool IsClosed(MapLocation marker)
+    {
+        foreach (PathMarker p in closed)
+        {
+            if (p.location.Equals(marker)) return true;
+        }
+        return false;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -50,6 +139,6 @@ public class FindPathAStar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P)) BeginSearch();
     }
 }
